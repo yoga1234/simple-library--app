640996d930cbac346dccf2b9013f3e9db7df195f
yoga1234 <septianayoga30@gmail.com>
2026-03-07 17:51:18 +0700

    finishin project and fixing array add bug

--- javascript.js
+++ javascript.js
@@ -1,11 +1,14 @@
 // handling the book data
 let bookData = [];
 let cardContainer = document.getElementById("cardContainer");
-
-// check if bookData is Empty
-if (!Array.isArray(bookData) || !bookData.length) {
-  arrayIsEmpty(cardContainer);
+// checking if array is empty
+function dataCheck() {
+  if (!Array.isArray(bookData) || !bookData.length) {
+    arrayIsEmpty(cardContainer);
+  }
 }
+
+dataCheck();
 
 // adding text to html if the array is empty
 function arrayIsEmpty(htmlElement) {
@@ -19,12 +22,15 @@ function arrayIsEmpty(htmlElement) {
 
 // object constructor
 function book(author, title, pages, isRead) {
+  let bookId = bookData.length;
+
+  this.bookId = bookId;
   this.author = author;
   this.title = title;
   this.pages = pages;
   this.isread = isRead;
-  console.log(author, title, pages, isread);
-  bookData.push({ author, title, pages, isRead });
+
+  bookData.push({ bookId, author, title, pages, isRead });
 }
 
 // removing submit default behaviour
@@ -33,32 +39,57 @@ document
   .addEventListener("click", function (event) {
     event.preventDefault();
 
-    // getting form data
-    const formAuthor = document.getElementById("author").value;
-    const formTitle = document.getElementById("title").value;
-    const formPages = document.getElementById("pages").value;
-    const formIsRead = document.getElementById("hasBeenRead").value;
+    showData();
+  });
 
-    book(formAuthor, formTitle, formPages, formIsRead);
+function showData() {
+  // fungsi ini berjalan ketika tombol tambah dan tombol delete dihapus
 
-    // reseting form after submit
-    document.getElementById("inputBook").reset();
+  // getting form data
+  const formAuthor = document.getElementById("author").value;
+  const formTitle = document.getElementById("title").value;
+  const formPages = document.getElementById("pages").value;
+  const formIsRead = document.getElementById("hasBeenRead").value;
 
-    // adding book data to the html
-    // crearing the div
-    cardContainer.innerHTML = "";
+  book(formAuthor, formTitle, formPages, formIsRead);
 
-    bookData.forEach((element) => {
-      let cardData = document.createElement("div");
-      cardData.className = "book-card";
-      cardData.innerHTML = `
+  // reseting form after submit
+  document.getElementById("inputBook").reset();
+
+  // crearing the div
+  cardContainer.innerHTML = "";
+
+  bookData.forEach((element) => {
+    let cardData = document.createElement("div");
+    cardData.className = "book-card";
+    cardData.innerHTML = `
       <h3>${element.title}</h3>
       <hr />
       <p>Author: ${element.author}</p>
       <p>Pages: ${element.pages}</p>
       <p>${element.isRead == "read" ? "Read" : "Not Read"}</p>
-      <button>Delete</button>`;
+      <button id="${element.bookId}" class="deleteBook">Delete</button>`;
 
-      cardContainer.insertAdjacentElement("afterbegin", cardData);
-    });
+    cardContainer.insertAdjacentElement("afterbegin", cardData);
   });
+
+  deleteBook();
+}
+
+// adding click event to a every button that has deleteBook class
+function deleteBook() {
+  let deleteBtns = document.querySelectorAll(".deleteBook");
+
+  for (var i = 0; i < bookData.length; i++) {
+    deleteBtns[i].addEventListener("click", function (e) {
+      const newBooks = bookData.filter(function (book) {
+        return book.bookId != e.target.id;
+      });
+      bookData = newBooks;
+
+      // delete in the dom
+      const card = e.target.closest(".book-card");
+      card.remove();
+    });
+  }
+}
